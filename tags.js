3b47252cd5a7641d3f1667026577effaaab37aa2
SL-BennettYau <b.yau@fusion92.com>
2023-10-24 17:35:42 -0500

    Update tags.js

--- tags.js
+++ tags.js
@@ -1,5 +1,4 @@
-let tags = {
-    "add_to_cart": {
+    let base = {
         "storeID": "{{storeID}}",
         "listingID": "{{listingID}}",
         "viewName": "{{viewName}}",
@@ -14,12 +13,33 @@ let tags = {
         "navMethod": "{{navMethod}}",
         "viewMode": "{{viewMode}}",
         "viewTitle": "{{viewTitle}}",
-        "sendIf": "{{sendIf}}",
         "content_group1": "{{content_group1}}",
         "page_path": "{{page_path}}",
         "event_label": "{{event_label}}",
-        "children": "{{children}}",
         "event_action": "{{event_action}}",
         "event_category": "{{event_category}}",
     }
-}
+
+    let localTags = {
+        "add_to_cart": {
+            "storeID": "{{storeID}}",
+            "listingID": "{{listingID}}",
+            "viewName": "{{viewName}}",
+            "language": "{{language}}",
+            "storeDMA": "{{storeDMA}}",
+            "storePostalCode": "{{storePostalCode}}",
+            "storeName": "{{storeName}}",
+            "retailerID": "{{retailerID}}",
+            "retailerName": "{{retailerName}}",
+            "siteName": "{{siteName}}",
+            "navOrigin": "{{navOrigin}}",
+            "navMethod": "{{navMethod}}",
+            "viewMode": "{{viewMode}}",
+            "viewTitle": "{{viewTitle}}",
+            "content_group1": "{{content_group1}}",
+            "page_path": "{{page_path}}",
+            "event_label": "{{event_label}}",
+            "event_action": "{{event_action}}",
+            "event_category": "{{event_category}}",
+        },
+    }
