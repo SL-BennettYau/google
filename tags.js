5ed520a778d75e0b56d141fd0016703611613969
SL-BennettYau <b.yau@fusion92.com>
2023-10-24 13:34:23 -0500

    Create tags.js

---
+++ tags.js
@@ -0,0 +1,25 @@
+let tags = {
+    "add_to_cart": {
+        "storeID": "{{storeID}}",
+        "listingID": "{{listingID}}",
+        "viewName": "{{viewName}}",
+        "language": "{{language}}",
+        "storeDMA": "{{storeDMA}}",
+        "storePostalCode": "{{storePostalCode}}",
+        "storeName": "{{storeName}}",
+        "retailerID": "{{retailerID}}",
+        "retailerName": "{{retailerName}}",
+        "siteName": "{{siteName}}",
+        "navOrigin": "{{navOrigin}}",
+        "navMethod": "{{navMethod}}",
+        "viewMode": "{{viewMode}}",
+        "viewTitle": "{{viewTitle}}",
+        "sendIf": "{{sendIf}}",
+        "content_group1": "{{content_group1}}",
+        "page_path": "{{page_path}}",
+        "event_label": "{{event_label}}",
+        "children": "{{children}}",
+        "event_action": "{{event_action}}",
+        "event_category": "{{event_category}}",
+    }
+}
