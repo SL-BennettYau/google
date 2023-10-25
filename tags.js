0e3ca9c7deabd776e1d05fcddfd43e2b8976e7e9
SL-BennettYau <b.yau@fusion92.com>
2023-10-25 14:48:37 -0500

    Update tags.js

--- tags.js
+++ tags.js
@@ -22,24 +22,7 @@
 
     let tags = {
         "add_to_cart": {
-            "storeID": "{{storeID}}",
-            "listingID": "{{listingID}}",
-            "viewName": "{{viewName}}",
-            "language": "{{language}}",
-            "storeDMA": "{{storeDMA}}",
-            "storePostalCode": "{{storePostalCode}}",
-            "storeName": "{{storeName}}",
-            "retailerID": "{{retailerID}}",
-            "retailerName": "{{retailerName}}",
-            "siteName": "{{siteName}}",
-            "navOrigin": "{{navOrigin}}",
-            "navMethod": "{{navMethod}}",
-            "viewMode": "{{viewMode}}",
-            "viewTitle": "{{viewTitle}}",
-            "content_group1": "{{content_group1}}",
-            "page_path": "{{page_path}}",
-            "event_label": "{{event_label}}",
-            "event_action": "{{event_action}}",
-            "event_category": "{{event_category}}",
+        },
+        "browse_category": {
         },
     }
